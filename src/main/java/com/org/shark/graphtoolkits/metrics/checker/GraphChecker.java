d0632281e0403eff7a2d6283ed0291e79a413066
Yingxia Shao <simon0227@gmail.com>
2015-10-05 15:45:56 +0800

    create the maven project of graph toolkit

---
+++ src/main/java/com/org/shark/graphtoolkits/metrics/checker/GraphChecker.java
@@ -0,0 +1,11 @@
+package com.org.shark.graphtoolkits.metrics.checker;
+
+/**
+ * Created by yxshao on 10/5/15.
+ */
+public class GraphChecker {
+
+    public static void main(String [] args) {
+       System.out.println("Welcome to the graph world!");
+    }
+}
